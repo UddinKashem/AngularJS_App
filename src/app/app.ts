207cfe423c2f70aeed7444e64727bfb160ac99e2
UddinKashem <30503392+UddinKashem@users.noreply.github.com>
2026-04-21 22:51:04 -0400

    adding component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,18 @@
+import { CoursesComponent } from './../courses/courses.component';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    CoursesComponent
+
+  ],
   template: `
     <h1>Hello, {{ title() }}</h1>
+    <h3>Learning AngularJS!</h3>
+    <courses>Courses</courses>
 
     <router-outlet />
   `,
